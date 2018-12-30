a840d54ed39484157ce5d19539d003e76b23d93a
Michael Roose <mjroose@gmail.com>
2018-12-30 15:35:13 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,3 +5,15 @@ function getFirstSelector(selector) {
 function nestedTarget() {
   return document.querySelector('#nested .target');
 }
+
+function deepestChild() {
+  
+}
+
+function increaseRankBy(n) {
+  const rls = document.querySelectorAll('.ranked-list li');
+
+  for(let i = 0; i < rls.length; i++) {
+    rls[i].innerHTML += 1;
+  }
+}
